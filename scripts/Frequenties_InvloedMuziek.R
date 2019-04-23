5665e704d557f554e3856b8ef515479c570ce05a
Robbe Decorte <robbe.decorte@student.hogent.be>
2019-04-23 12:21:28 +0200

    frequentiegrafieken en boxplot voor invloed muziek

--- scripts/Frequenties_InvloedMuziek.R
+++ scripts/Frequenties_InvloedMuziek.R
@@ -12,6 +12,8 @@ experiment.variables <- experiment.data[variables]
 experiment.variables <- subset(experiment.variables, score_1 != "" & score_2 != "" & frq_cannabis != "")
 
 dev.new()
-boxplot(experiment.variables$score_1~experiment.variables$muziek, horizontal = TRUE, main = "Aantal onthouden feiten", ylab ="Luistern naar Mozart")
+boxplot(experiment.variables$score_1~experiment.variables$muziek, horizontal = TRUE, main = "Invloed van muziek", ylab ="Luistern naar Mozart")
 dev.new()
-hist(experiment.variables$score_1, main = "Aantal herinnerde feiten", xlab = "", breaks = 2)
+hist(experiment.variables$score_1, main = "Frequentie van 1e ronde", xlab = "Aantal herinnerde feiten", breaks = 4)
+dev.new()
+hist(experiment.variables$score_2, main = "Frequentie van 2e ronde", xlab = "Aantal herinnerde feiten", breaks = 4)
